ceb1a03ed1201ad447be76ea8387eec13783d173
Mario Martinez <martimatix@Marios-MacBook-Pro.local>
2015-02-07 14:40:43 +1100

    roll creates new turn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,29 @@
 class GamesController < ApplicationController
   def new
+    @dice = roll_dice
+    Turn.create(dice_hash(@dice))
+
+    @fields = %w(ones twos threes fours fives sixes
+                 subtotal bonus upper_score_total
+                 three_of_a_kind four_of_a_kind full_house
+                 small_straight large_straight rahtzee)
+  end
+
+  def roll_again
+    redirect_to(games_new_path)
+  end
+
+  private
+  def roll_dice(dice = [true]*5)
+    dice.map! {|d| rand(1..6) if d}
+    dice
+  end
+
+  def dice_hash(dice)
+    hash = {}
+    (1..5).each do |i|
+      hash["dice_#{i}".to_sym] = dice[i]
+    end
+    hash
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root :to => 'games#new'
 
   get '/games/new' => 'games#new'
+  #will need to change this to /games/:id/roll_dice later
+  post '/games/roll_again' => 'games#roll_again'
+
 
 end
