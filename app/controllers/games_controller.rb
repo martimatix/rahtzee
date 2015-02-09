ecd4c8730e94c71d4582f8f99d82fa46a74f4867
Mario Martinez <zenitram.oiram@gmail.com>
2015-02-09 14:26:11 +1100

    Distinguished private methods

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -35,7 +35,9 @@ class GamesController < ApplicationController
 
     score_field = params['score_field']
 
-    if score_field.nil?
+    if @game.filled
+      redirect_to games_game_over_path
+    elsif score_field.nil?
       render "game" 
       return
     end
@@ -46,16 +48,12 @@ class GamesController < ApplicationController
     @game.send(score_field + '=', score)
     @game.save
 
-    update_score
+    update_score_sheet
 
-    if game_over?
-      redirect_to games_game_over_path
-    else
-      dice = roll_dice
-      @turn = Turn.create(dice_hash(dice)) 
-      redirect_to :controller => 'games', :action => 'new_turn',
-                  :game_id => @game.id, :turn_id => @turn.id
-    end
+    dice = roll_dice
+    @turn = Turn.create(dice_hash(dice)) 
+    redirect_to :controller => 'games', :action => 'new_turn',
+                :game_id => @game.id, :turn_id => @turn.id
   end
 
   def new_turn
@@ -69,69 +67,72 @@ class GamesController < ApplicationController
   def game_over
   end
 
-  private
-  def roll_dice(dice = [true]*5)
-    dice.map! {|d| rand(1..6) if d}
-    dice
-  end
+  ########################### Private Methods ###########################
 
-  def dice_hash(dice)
-    hash = {}
-    (1..5).each do |i|
-      hash["dice_#{i}".to_sym] = dice[i-1]
+    private
+    def roll_dice(dice = [true]*5)
+      dice.map! {|d| rand(1..6) if d}
+      dice
     end
-    hash
-  end
 
-  def dice_check(params)
-    dice_to_roll = []
-    (1..5).each do |i|
-      dice_to_roll[i - 1] = params.keys.include? "dice_#{i}"
+    def dice_hash(dice)
+      hash = {}
+      (1..5).each do |i|
+        hash["dice_#{i}".to_sym] = dice[i-1]
+      end
+      hash
     end
-    dice_to_roll
-  end
 
-  def extract_dice(turn)
-    dice = []
-    (1..5).to_a.each { |i| dice << eval("turn.dice_#{i}") }
-    dice
-  end
+    def dice_check(params)
+      dice_to_roll = []
+      (1..5).each do |i|
+        dice_to_roll[i - 1] = params.keys.include? "dice_#{i}"
+      end
+      dice_to_roll
+    end
 
-  # Raw upper score total (no bonus)
-  def raw_upper
-    upper_values = Game.upper_scores.map {|f| @game.send(f).to_i }
-    @game.raw_upper = upper_values.reduce(:+)
-  end
+    def extract_dice(turn)
+      dice = []
+      (1..5).to_a.each { |i| dice << eval("turn.dice_#{i}") }
+      dice
+    end
 
-  def upper_score_bonus
-    @game.upper_score_bonus = 35 if @game.raw_upper > 62
-  end
+    # Raw upper score total (no bonus)
+    def raw_upper
+      upper_values = Game.upper_scores.map {|f| @game.send(f).to_i }
+      @game.raw_upper = upper_values.reduce(:+)
+    end
 
-  def upper_score
-    @game.upper_score = @game.raw_upper + @game.upper_score_bonus
-  end
+    def upper_score_bonus
+      @game.upper_score_bonus = 35 if @game.raw_upper > 62
+    end
 
-  def lower_score
-    lower_values = Game.lower_scores.map {|f| @game.send(f).to_i }
-    @game.lower_score = lower_values.reduce(:+)
-  end
+    def upper_score
+      @game.upper_score = @game.raw_upper + @game.upper_score_bonus
+    end
 
-  def total_score
-    @game.total_score = @game.upper_score + @game.lower_score
-  end
+    def lower_score
+      lower_values = Game.lower_scores.map {|f| @game.send(f).to_i }
+      @game.lower_score = lower_values.reduce(:+)
+    end
 
-  def update_score
-    raw_upper
-    upper_score_bonus
-    upper_score
-    lower_score
-    total_score
-    @game.save
-  end
+    def total_score
+      @game.total_score = @game.upper_score + @game.lower_score
+    end
 
-  def game_over?
-    game = Game.find params[:game_id]
-    Game.fields.all? {|f| game.send f}
-  end 
+    def update_score_sheet
+      raw_upper
+      upper_score_bonus
+      upper_score
+      lower_score
+      total_score
+      @game.filled = true if game_over?
+      @game.save
+    end
+
+    def game_over?
+      game = Game.find params[:game_id]
+      Game.fields.all? {|f| game.send f}
+    end 
       
 end
