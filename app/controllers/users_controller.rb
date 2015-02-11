ca0542325c621ef695ef21b925b018b5cf2203f1
Mario Martinez <zenitram.oiram@gmail.com>
2015-02-11 16:04:24 +1100

    edit user working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,16 @@ class UsersController < ApplicationController
     @user = User.find params[:id]
   end
 
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    user = User.find params[:id]
+    user.update user_params
+    redirect_to user
+  end
+
   def index
     @users = User.all
   end
