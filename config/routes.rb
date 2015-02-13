1485691a7884cba77072f3a9830b564790bd3d1e
Mario Martinez <zenitram.oiram@gmail.com>
2015-02-13 11:44:02 +1100

    cleaned up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
   resources :users, :only => [:new, :create, :show, :edit, :update, :index]
 
   get '/games/new' => 'games#new'
-  # to do: add user id to path for greater verification
   post '/games/:game_id/roll_again' => 'games#roll_again'
   post '/games/:game_id/enter_score' => 'games#enter_score'
-  # todo: merge games#new_turn with games#new
   get '/games/:game_id/new_turn' => 'games#new_turn'
   get '/games/:game_id/game_over' => 'games#game_over'
 
