aa917b19879035a88e75b0f7607235dc94675881
Mario Martinez <zenitram.oiram@gmail.com>
2015-02-16 12:11:43 +1100

    users cannot edit other user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,11 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find params[:id]
+    if @user == @current_user
+      render :edit
+    else
+      redirect_to @user
+    end
   end
 
   def update
